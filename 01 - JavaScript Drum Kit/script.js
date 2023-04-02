0f9072f7e28e71b49e7e6bd8433f4acead264447
Salov Ivan <singlelol21@gmail.com>
2023-04-02 13:33:53 +0300

    fix: ass js code to 01-JavascriptDrumKit

---
+++ 01 - JavaScript Drum Kit/script.js
@@ -0,0 +1,24 @@
+const keys = document.querySelector('.keys');
+function endTransition(e) {
+  e.target.classList.remove('playing')
+}
+
+function playSound(e) {
+  const target = e instanceof Node ? e.getAttribute('data-key') : '';
+  const keyTarget = document.querySelector(`div[data-key="${e.keyCode || target}"]`);
+  const audioTarget = document.querySelector(`audio[data-key="${e.keyCode || target}"]`);
+  if(!audioTarget) return;
+
+  keyTarget.classList.add('playing');
+  audioTarget.currentTime = 0;
+  audioTarget.play();
+}
+
+window.addEventListener('keydown', playSound)
+keys.addEventListener('click', (event) => {
+  playSound(event.target.closest('div'))
+})
+
+Array.from(keys.childNodes).filter(el => el.classList == 'key').forEach(el => {
+  el.addEventListener('transitionend', endTransition)
+});
